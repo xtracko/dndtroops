a3bd86dee713a4e56438a4a0483dd562700a0469
Jiří Novotný <xtracko@users.noreply.github.com>
2017-10-26 14:55:14 +0200

    Role entity and Role DAO implemented

--- dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/RoleDao.java
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/RoleDao.java
@@ -1,7 +1,54 @@
+/**
+ * @author Jiří Novotný
+ */
+
 package cz.muni.fi.pa165.dndtroops.dao;
 
-public class RoleDao {
+import cz.muni.fi.pa165.dndtroops.entities.Role;
 
-    // TODO
+import java.util.List;
+
+/**
+ * Data Access Object for Role entity.
+ */
+public interface RoleDao {
+
+    /**
+     * Persist a Role.
+     *
+     * @param role a Role to persist
+     */
+    void createRole(Role role);
+
+    /**
+     * Delete persisted Role.
+     *
+     * @param role a persisted Role to delete
+     */
+    void deleteRole(Role role);
+
+    /**
+     * Find persisted Role by it's unique ID. If no such role is found null is returned.
+     *
+     * @param id an ID of a Role to find
+     * @return Role with the required ID or null if such Role does not exists
+     */
+    Role findRoleById(Long id);
+
+    /**
+     * Find persisted Role by it's unique name. If no such role is found null is returned.
+     *
+     * @param name a name of a Role to find
+     * @return Role with the required name or null if such Role does not exists
+     */
+    Role findRoleByName(String name);
+
+
+    /**
+     * Find all persisted Roles and return them as a List.
+     *
+     * @return List of all persisted Roles
+     */
+    List<Role> findAllRoles();
 
 }
