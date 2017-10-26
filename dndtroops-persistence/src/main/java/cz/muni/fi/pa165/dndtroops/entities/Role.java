6013ab9de60f4a7e76c20060deb804660a45a29a
Jiří Novotný <novotng@gmail.com>
2017-10-26 08:47:34 +0200

    Initial Maven project definition (#1)
    * Maven dndtroops and dndtroops-persistence projects defined
    * Populate repository with project skeleton

---
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/entities/Role.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.dndtroops.entities;
+
+public class Role {
+
+    // TODO
+
+}
---
+++ dndtroops-persistence/src/test/java/cz/muni/fi/pa165/dndtroops/dao/AdministratorDaoTest.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.dndtroops.dao;
+
+import cz.muni.fi.pa165.dndtroops.PersistenceSampleApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class AdministratorDaoTest extends AbstractTestNGSpringContextTests {
+
+    // TODO
+
+}
