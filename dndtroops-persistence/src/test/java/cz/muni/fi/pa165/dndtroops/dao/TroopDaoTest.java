de2f5d03dc05afaf229e77d000f68b04969c59e9
xsestak4 <410204@mail.muni.cz>
2017-10-28 11:15:18 +0200

    Update TroopDaoTest.java

--- dndtroops-persistence/src/test/java/cz/muni/fi/pa165/dndtroops/dao/TroopDaoTest.java
+++ dndtroops-persistence/src/test/java/cz/muni/fi/pa165/dndtroops/dao/TroopDaoTest.java
@@ -1,19 +1,12 @@
 package cz.muni.fi.pa165.dndtroops.dao;
 
 import cz.muni.fi.pa165.dndtroops.PersistenceSampleApplicationContext;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
 public class TroopDaoTest extends AbstractTestNGSpringContextTests {
 
-    @PersistenceContext
-	public EntityManager em;
-
-	@Autowired
-	public TroopDao productDao;
+    // TODO
 
 }
