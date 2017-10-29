4ca904c50bc4b2174af101f1903353ff9bd6bb36
Vojtech Duchon <vojtech.duchon@gmail.com>
2017-10-29 16:53:47 +0100

    Troop hash and equals methods edited, money value changed back to long, added notnull annotations

--- dndtroops-persistence/src/test/java/cz/muni/fi/pa165/dndtroops/dao/HeroDaoTest.java
+++ dndtroops-persistence/src/test/java/cz/muni/fi/pa165/dndtroops/dao/HeroDaoTest.java
@@ -2,11 +2,15 @@ package cz.muni.fi.pa165.dndtroops.dao;
 
 import cz.muni.fi.pa165.dndtroops.PersistenceSampleApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
 public class HeroDaoTest extends AbstractTestNGSpringContextTests {
 
-    // TODO
 
 }
