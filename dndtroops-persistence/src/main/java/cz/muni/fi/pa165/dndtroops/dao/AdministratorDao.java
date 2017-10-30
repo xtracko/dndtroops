63ef1e597559ae96f1f7c59dcd40cea2a684de15
Miroslav Mačor <miroslav.macor@gmail.com>
2017-10-30 18:38:50 +0100

    Updating javadoc and changing equals method to instance of

--- dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/AdministratorDao.java
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/AdministratorDao.java
@@ -10,17 +10,42 @@ import java.util.List;
 
 public interface AdministratorDao {
 
+    /**
+     * Creates new Administrator
+     * @param admin
+     */
+    void createAdministrator(Administrator admin);
 
-    public void createAdministrator(Administrator admin);
+    /**
+     * Changes Administrator with the same admin.id. Changes only values that differ
+     * @param admin
+     */
+    void updateAdministrator(Administrator admin);
 
-    public void updateAdministrator(Administrator admin);
+    /**
+     * Returns Administrator with the id or null
+     * @param id of Administrator
+     * @return Administrator with sanme id
+     */
+    Administrator findAdministatorById(Long id);
 
-    public Administrator findAdministatorById(Long id);
+    /**
+     * Finds Administrator by name
+     * @param name of Administrator
+     * @return Administrator with same name or null
+     */
+    Administrator findAdminByName(String name);
 
-    public Administrator findAdminByName(String name);
+    /**
+     * Get all Administrators
+     * @return all current Administrator
+     */
+    List<Administrator> findAllAdministrators();
 
-    public List<Administrator> findAllAdministrators();
-
-    public void removeAdministrator(Administrator admin);
+    /**
+     * If exist removes Administrator
+     * @param admin to be removed
+     */
+    void removeAdministrator(Administrator admin);
 
 }
