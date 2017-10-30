81aeb86c9e5f67da4b35b41a45090db609516279
Miroslav Mačor <miroslav.macor@gmail.com>
2017-10-30 09:14:16 +0100

    Addding administrator and administratorDao implementationtemp

--- dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/AdministratorDao.java
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/AdministratorDao.java
@@ -1,7 +1,26 @@
 package cz.muni.fi.pa165.dndtroops.dao;
 
-public class AdministratorDao {
+import cz.muni.fi.pa165.dndtroops.entities.Administrator;
 
-    // TODO
+import java.util.List;
+
+/**
+ * Creted by Miroslav Macor
+ */
+
+public interface AdministratorDao {
+
+
+    public void createAdministrator(Administrator admin);
+
+    public void updateAdministrator(Administrator admin);
+
+    public Administrator findAdministatorById(Long id);
+
+    public Administrator findAdminByName(String name);
+
+    public List<Administrator> findAllAdministrators();
+
+    public void removeAdministrator(Administrator admin);
 
 }
