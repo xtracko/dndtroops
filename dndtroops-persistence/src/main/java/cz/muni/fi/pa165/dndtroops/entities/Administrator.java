345c74f68c8e1500b7615c3b7788898b974d37b1
Miroslav Mačor <miroslav.macor@gmail.com>
2017-10-30 09:14:16 +0100

    Addding administrator and administratorDao implementationtemp

--- dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/entities/Administrator.java
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/entities/Administrator.java
@@ -1,7 +1,47 @@
 package cz.muni.fi.pa165.dndtroops.entities;
 
+/**
+ * Created by Miroslav Macor
+ */
 public class Administrator {
+    private String name;
+    private long id;
 
-    // TODO
 
+    public Administrator(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Administrator admin = (Administrator) o;
+
+        if (!getName().equals(admin.getName())) return false;
+        return  (getId() == (admin.getId()));
+    }
+
+    @Override
+    public int hashCode() {
+        return 67 * getName().hashCode();
+    }
 }
