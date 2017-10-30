5f07116a28f13582a496fa5ccf89adcb603781c8
Vojtech Duchon <vojtech.duchon@gmail.com>
2017-10-30 22:59:44 +0100

    Added JavaDoc

--- dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/TroopDao.java
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/dao/TroopDao.java
@@ -5,19 +5,52 @@ import cz.muni.fi.pa165.dndtroops.entities.Troop;
 import java.util.List;
 
 /*
-
-DAO class for entity Troop.
-Creted by: Vojtech Duchon (UCO:410007)
-
+*@author Vojtech Duchoň
  */
 
 public interface TroopDao {
-
+    /*
+    * Presist Troop.
+    *
+    * @param Troop to be persisted
+     */
     public void create (Troop t );
+
+    /*
+    * Remove persisted Troop.
+    *
+    * @param Troop to be removed.
+    */
     public void delete (Troop t );
+
+    /*
+    * Merge the state of the given entity into the current persistence context.
+    *
+    * @param Troop to be updated.
+    */
     public void update (Troop t );
+
+    /*
+    * Finds troop by given troop ID. Returns null when no troop found with given ID.
+    *
+    * @param ID of the searched troop
+    * @return Troop if troop with given ID exists, if not returns null
+     */
     public Troop findById (Long id);
+
+    /*
+    * Finds troop by given name of the troop. If troop with given name is not persisted, returns null.
+    *
+    * @param String with name of the searched troop.
+    * @return Troop with given name or null if not persisted.
+     */
     public Troop findByName (String name);
+
+    /*
+   * Retrieves a list of all persisted troops.
+   *
+   * @return Troop with given name or null if not persisted.
+    */
     public List<Troop> findAll();
 
 }
