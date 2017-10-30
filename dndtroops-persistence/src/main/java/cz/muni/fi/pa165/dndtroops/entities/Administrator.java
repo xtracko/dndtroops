965317d43dbc6e2d97b8098178db2a5ed027663a
Miroslav Mačor <miroslav.macor@gmail.com>
2017-10-30 18:38:50 +0100

    Updating javadoc and changing equals method to instance of

--- dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/entities/Administrator.java
+++ dndtroops-persistence/src/main/java/cz/muni/fi/pa165/dndtroops/entities/Administrator.java
@@ -32,7 +32,7 @@ public class Administrator {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof  Administrator)) return false;
 
         Administrator admin = (Administrator) o;
 
