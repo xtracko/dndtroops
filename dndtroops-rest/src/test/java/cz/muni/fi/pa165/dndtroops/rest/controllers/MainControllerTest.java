70de8642d0e84be599cc1208fa18c652cae57c8e
Jiří Novotný <xtracko@users.noreply.github.com>
2017-12-11 14:16:31 +0100

    REST modeule dependency fix

--- dndtroops-rest/src/test/java/cz/muni/fi/pa165/dndtroops/rest/controllers/MainControllerTest.java
+++ dndtroops-rest/src/test/java/cz/muni/fi/pa165/dndtroops/rest/controllers/MainControllerTest.java
@@ -2,12 +2,12 @@ package cz.muni.fi.pa165.dndtroops.rest.controllers;
 
 import cz.muni.fi.pa165.dndtroops.RootWebContext;
 import cz.muni.fi.pa165.dndtroops.rest.ApiUris;
-import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testng.annotations.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
